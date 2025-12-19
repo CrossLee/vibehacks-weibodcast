32803fcd3e04f1a23f4d890e183102976316b48a
cross <mag_lee@126.com>
2025-12-20 00:28:33 +0800

    add: 继续生成常量

---
+++ constants.ts
@@ -0,0 +1,5 @@
+export const WEIBO_PRESETS = [
+  { name: '罗永浩', id: '1997678024236994771', filename: 'voice-source/罗永浩.wav' },
+  { name: '何广智', id: '5907116391', filename: 'voice-source/何广智.m4a' },
+  { name: '徐志胜', id: '5687043680', filename: 'voice-source/徐志胜.mp3' }
+];
