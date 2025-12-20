2e17420f9d79774c4c485773ad5ad8bba04bada5
cross <mag_lee@126.com>
2025-12-20 16:56:43 +0800

    删除微博用户下拉框中的徐志胜

--- constants.ts
+++ constants.ts
@@ -1,5 +1,4 @@
 export const WEIBO_PRESETS = [
   { name: '罗永浩', id: '1997678024236994771', filename: 'voice-source/罗永浩.wav' },
-  { name: '何广智', id: '5907116391', filename: 'voice-source/何广智.m4a' },
-  { name: '徐志胜', id: '5687043680', filename: 'voice-source/徐志胜.mp3' }
+  { name: '何广智', id: '5907116391', filename: 'voice-source/何广智.m4a' }
 ];
